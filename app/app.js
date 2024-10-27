33babee2d407bf072ecfd93111dbf6c4e136d11e
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-27 22:06:33 +0100

    An update

--- app/app.js
+++ app/app.js
@@ -1,14 +1,15 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import dbConnect from "../config/dbConnect.js";
 import userRoutes from "../routers/usersRoute.js";
 //db connect
 dbConnect();
 const app = express();
-
+//pass incoming data
+app.use(express.json());
 //routes
 
-app.use("/" , userRoutes);
+app.use("/", userRoutes);
 
 export default app;
