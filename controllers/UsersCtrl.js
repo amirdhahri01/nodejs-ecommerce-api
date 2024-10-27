7338ef0d98b5e570c5e131e09de197848cf3c07b
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-27 22:40:04 +0100

    An update

--- controllers/UsersCtrl.js
+++ controllers/UsersCtrl.js
@@ -1,7 +1,7 @@
 import User from "../model/User.js";
 import bcrypt from "bcryptjs";
 // @desc   Register user
-// @route  Post /api/v1/users/register
+// @route  POST /api/v1/users/register
 // @access Private/Admin
 
 export const registerUserCtrl = async (req, res) => {
@@ -11,15 +11,34 @@ export const registerUserCtrl = async (req, res) => {
   if (userExists) {
     //throw
     res.json({ msg: "User already exists" });
-  } 
+  }
   //hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   //create the user
-  const user = await User.create({ fullname, email, "password":hashedPassword });
+  const user = await User.create({ fullname, email, password: hashedPassword });
   res.status(201).json({
     status: "success",
-    message: "User Registered Successfully",
+    message: "User registered successfully",
     data: user,
   });
+};
+
+// @desc   Login user
+// @route  POST /api/v1/users/login
+// @access Public
+
+export const loginUserCtrl = async (req, res) => {
+  const { email, password } = req.body;
+  // find user in db by email only
+  const userFound = await User.findOne({ email });
+  if (userFound && (await bcrypt.compare(password, userFound?.password))) {
+    res.json({
+      status: "success",
+      message: "User logged in successfully",
+      userFound,
+    });
+  } else {
+    res.json({ msg: "Invalid login details" });
+  }
 };
--- routers/usersRoute.js
+++ routers/usersRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerUserCtrl } from "../controllers/UsersCtrl.js";
+import { registerUserCtrl , loginUserCtrl} from "../controllers/UsersCtrl.js";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/api/v1/users/register" , registerUserCtrl);
+userRoutes.post("/api/v1/users/login" , loginUserCtrl);
 
 export default userRoutes;
