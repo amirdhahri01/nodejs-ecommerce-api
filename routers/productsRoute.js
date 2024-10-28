5f7e21dbc1dc5f3d866c6395a0a3586a6d92f409
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-28 14:19:22 +0100

    An update

--- routers/productsRoute.js
+++ routers/productsRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createProductCtrl } from "../controllers/ProductCtrl.js";
+import {
+  createProductCtrl,
+  getProductsCtrl,
+} from "../controllers/ProductCtrl.js";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
 const productRoutes = express.Router();
 
 productRoutes.post("/", isLoggedIn, createProductCtrl);
+productRoutes.get("/", getProductsCtrl);
 
 export default productRoutes;
