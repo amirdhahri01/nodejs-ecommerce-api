1adb6d918afe3c96b8273bfa1c75df71c439ebcd
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-31 12:28:23 +0100

    An update

--- app/app.js
+++ app/app.js
@@ -10,20 +10,54 @@ import brandRoutes from "../routers/brandsRouter.js";
 import colorRoutes from "../routers/colorsRouter.js";
 import reviewRoutes from "../routers/reviewsRouter.js";
 import orderRoutes from "../routers/ordersRouter.js";
+import Stripe from "stripe";
 //db connect
 dbConnect();
 const app = express();
-//pass incoming data
+//Pass incoming data
 app.use(express.json());
-//routes
+//Routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/products", productRoutes);
-app.use("/api/v1/categories" , categoryRoutes)
-app.use("/api/v1/brands" , brandRoutes)
-app.use("/api/v1/colors" , colorRoutes)
-app.use("/api/v1/reviews" , reviewRoutes)
-app.use("/api/v1/orders", orderRoutes)
-//err middleware
+app.use("/api/v1/categories", categoryRoutes);
+app.use("/api/v1/brands", brandRoutes);
+app.use("/api/v1/colors", colorRoutes);
+app.use("/api/v1/reviews", reviewRoutes);
+app.use("/api/v1/orders", orderRoutes);
+//Stripe webhook
+const stripe = new Stripe(process.env.STRIPE_KEY);
+const endpointSecret = "whsec_...";
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        console.log("PaymentIntent was successful!");
+        break;
+      case "payment_method.attached":
+        const paymentMethod = event.data.object;
+        console.log("PaymentMethod was attached to a Customer!");
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    response.json({ received: true });
+  }
+);
+app.listen(4242, () => console.log("Running on port 4242"));
+//Err middleware
 app.use(notFound);
 app.use(globalErrHandler);
 export default app;
