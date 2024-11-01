0a8e0c7330aa05f5b4f7a2396f7ccfb1b74a41c8
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-01 18:50:09 +0100

    An update

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import http from "http"
 import app from "./app/app.js"
 
 //Create the server
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 4000;
 const server = http.createServer(app)
 server.listen(PORT ,  console.log(`Server is running on http://localhost:${PORT}`));
