5607d91c89d002036d8d0c75cd55b0dbec008ad1
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-19 14:40:58 +0100

    An update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    const connected = await mongoose.connect(process.env.MONGO_URL);
+    const connected = await mongoose.connect(process.env.MONGO_URL_ATLAS);
     console.log(`Mongodb connected ${connected.connection.host}`);
   } catch (err) {
     console.log(`Error : ${err.message}`);
     process.exit(1);
   }
 };
- 
+
 export default dbConnect;
